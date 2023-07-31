64497951a2745f28f92479b7b78c134394547440
naresh <maddurinaresh7@gmail.com>
2023-07-31 20:41:33 +0530

    first commit

--- bill split/app.js
+++ bill split/app.js
@@ -11,15 +11,24 @@ document.getElementById("last").value="Each Person Should Pay : "+result.toFixed
 
  if(amount2 == 0 || amount2 == ""){
     document.getElementById("one").innerText=" Please enter valid details"
-    document.getElementById("last").value=""
+   //  document.getElementById("last").value="";
+   document.getElementById("persons").value=persons2;
+    document.getElementById("amount").style.borderColor = "red"
  }
+
  else if(persons2 == 0 || persons2 == ""){
     document.getElementById("two").innerText="Please enter valid details"
-    document.getElementById("last").value=""
+   //  document.getElementById("last").value=""
+   document.getElementById("amount").value=amount2;;
+    document.getElementById("persons").style.borderColor="red"
  }
+ 
  else{
     document.getElementById("one").innerText=""
     document.getElementById("two").innerText=""
+
+    document.getElementById("amount").style.borderColor = "green"
+    document.getElementById("persons").style.borderColor="green"
  }
 
  
